cdba46de16ac9ae4a2b29e214c721cc4ce3ea173
pamiii12 <fahmiadityaa03@gmail.com>
2023-06-22 10:46:00 +0700

    jika posisi rear diakhir

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -29,11 +29,12 @@ public:
 			REAR = 0;
 		}
 		else {
-			// Jika REAR berada di posisi terakhir array, kembali ke
+			// Jika REAR berada di posisi terakhir array, kembali ke awal array
 			if (REAR == max - 1)
 				REAR = 0;
 			else
 				REAR = REAR + 1;
 		}
+		queues_array[REAR] = num;
 	}
 };
